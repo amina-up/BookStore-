5c9173d9977641053fe0a269e92546d036e5113f
Amina Laabidi <53490773+amina-up@users.noreply.github.com>
2021-06-07 02:08:38 +0200

    installation of dependencies and create a server with node js

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express=require('express');
+const cors=require('cors')
+
+const app=express();
+app.use(express.json());
+app.use(cors())
+
+
+
+app.listen(3000,()=>{
+    console.log("Server start....")
+})
